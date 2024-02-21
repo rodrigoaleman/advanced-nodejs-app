f25fd8afc81533dae31feb9668e3858186c0fb97
Rodrigo Aleman <rodrigoal9715@gmail.com>
2024-02-21 00:37:38 -0600

    Add caching capability for blog data retrieval
    This change adds caching functionality to the `/api/blogs` route handler in `routes/blogRoutes.js`. It introduces the use of Redis to store and retrieve cached blog data.
    
    A Redis client is created with the connection URL `redis://127.0.0.1:6379`. The Redis client is then used to check if there is any cached data related to the user making the request (`req.user.id`). If there is cached data, it is immediately sent as the response. Otherwise, the route handler queries the MongoDB database for the user's blogs and stores the result in the Redis cache. The retrieved blogs are then sent as the response.
    
    This caching mechanism improves the performance of the `/api/blogs` route by serving cached data when available, reducing the need for repeated database queries.

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -4,35 +4,53 @@ const requireLogin = require('../middlewares/requireLogin');
 const Blog = mongoose.model('Blog');
 
 module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
-    const blog = await Blog.findOne({
-      _user: req.user.id,
-      _id: req.params.id
+    app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+        const blog = await Blog.findOne({
+            _user: req.user.id,
+            _id: req.params.id
+        });
+
+        res.send(blog);
     });
 
-    res.send(blog);
-  });
+    app.get('/api/blogs', requireLogin, async (req, res) => {
+        const redis = require('redis');
+        const redisUrl = 'redis://127.0.0.1:6379';
+        const client = redis.createClient(redisUrl);
+        const util = require('util');
+        client.get = util.promisify(client.get);
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+        /* Do we have any cached data in redis related to this query */
+        const cachedBlogs = await client.get(req.user.id);
 
-    res.send(blogs);
-  });
+        /* if yes, then respond to the request right away and return */
+        if (cachedBlogs) {
+            console.log('Serving from redis');
+            return res.send(JSON.parse(cachedBlogs));
+        }
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
-    const { title, content } = req.body;
+        /* if no, we need to respond to request and update our cache to store the data */
+        const blogs = await Blog.find({_user: req.user.id});
+        client.set(req.user.id, JSON.stringify(blogs));
 
-    const blog = new Blog({
-      title,
-      content,
-      _user: req.user.id
+        console.log('serving from mongo');
+        res.send(blogs);
     });
 
-    try {
-      await blog.save();
-      res.send(blog);
-    } catch (err) {
-      res.send(400, err);
-    }
-  });
+    app.post('/api/blogs', requireLogin, async (req, res) => {
+        const {title, content} = req.body;
+
+        const blog = new Blog({
+            title,
+            content,
+            _user: req.user.id
+        });
+
+        try {
+            await blog.save();
+            res.send(blog);
+        } catch (err) {
+            res.send(400, err);
+        }
+    });
 };
