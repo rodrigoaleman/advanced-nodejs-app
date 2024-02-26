fda37816ae263269df3601379bcfbd196b97aba2
Rodrigo Aleman <rodrigoal9715@gmail.com>
2024-02-25 22:48:23 -0600

    Add cache functionality to blog routes
    Implemented a caching mechanism for the `/api/blogs` route in `routes/blogRoutes.js` to enhance performance. Instead of querying the database every time, the route now checks if there is cached data in Redis related to the user's ID. If the data is found, it is served as the response. If not, the route fetches the blogs from the MongoDB database, stores them in the cache, and responds with the fetched blogs. This caching functionality was added by creating a new file, `services/cache.js`, which includes the necessary code for caching and retrieving data from Redis.
    
    New file: `services/cache.js`

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,26 +14,8 @@ module.exports = app => {
     });
 
     app.get('/api/blogs', requireLogin, async (req, res) => {
-        const redis = require('redis');
-        const redisUrl = 'redis://127.0.0.1:6379';
-        const client = redis.createClient(redisUrl);
-        const util = require('util');
-        client.get = util.promisify(client.get);
+        const blogs = await Blog.find({_user: req.user.id}).cache();
 
-        /* Do we have any cached data in redis related to this query */
-        const cachedBlogs = await client.get(req.user.id);
-
-        /* if yes, then respond to the request right away and return */
-        if (cachedBlogs) {
-            console.log('Serving from redis');
-            return res.send(JSON.parse(cachedBlogs));
-        }
-
-        /* if no, we need to respond to request and update our cache to store the data */
-        const blogs = await Blog.find({_user: req.user.id});
-        client.set(req.user.id, JSON.stringify(blogs));
-
-        console.log('serving from mongo');
         res.send(blogs);
     });
 
