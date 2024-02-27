2c2645f327849f382453e2fd476089f179a7ae35
Rodrigo Aleman <rodrigoal9715@gmail.com>
2024-02-26 22:52:55 -0600

    Add new test for logout button visibility
    The test suite `header.test.js` has been expanded by adding new dependencies and a new test case. This particular test case verifies that once the user is signed in, a logout button becomes visible. It does this by simulating a logged-in situation and checking for the presence of the logout button.

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const keygrip = require("keygrip");
+const {log} = require("nodemon/lib/utils");
 
 let browser, page;
 beforeEach(async () => {
@@ -13,13 +15,39 @@ afterEach(async () => {
     await browser.close();
 }, 120000);
 
+
 test('The header has the correct text', async () => {
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
     expect(text).toEqual('Blogster');
 });
 
 test('clicking login starts oauth flow', async () => {
-    await  page.click('.right a');
+    await page.click('.right a');
     const url = page.url();
     expect(url).toMatch(/accounts\.google\.com/);
 });
+
+test('When signed in, shows logout button', async () => {
+    const id = '65ce31ee88384d095f432428';
+
+    const Buffer = require('safe-buffer').Buffer;
+    const sessionObject = {
+        passport: {
+            user: id
+        }
+    };
+    const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
+
+    const Keygrip = require('keygrip');
+    const keys = require('../config/keys');
+    const keygrip = new Keygrip([keys.cookieKey]);
+    const sig = keygrip.sign('session=' + sessionString);
+
+    await page.setCookie({name: 'session', value: sessionString});
+    await page.setCookie({name: 'session.sig', value: sig});
+
+    await page.goto('localhost:3000');
+    await page.waitFor('a[href="/auth/logout"]');
+    const logoutText = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+    expect(logoutText).toEqual('Logout');
+})
