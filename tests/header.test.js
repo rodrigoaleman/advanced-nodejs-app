d7cbba2bbc2129b88aa060e8430f0ca7bf3e5466
Rodrigo Aleman <rodrigoal9715@gmail.com>
2024-02-27 23:26:10 -0600

    Refactor tests and add new helpers
    Created new test helper files (userFactory.js, sessionFactory.js, page.js) to simplify and improve code reuse in test cases. Refactored test codes to make use of these new helpers. We also updated the Jest configuration in package.json file and a database setup file for the test environment. These modifications lead to more maintainable and easier-to-read tests.

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,22 +1,19 @@
-const puppeteer = require('puppeteer');
-const keygrip = require("keygrip");
-const {log} = require("nodemon/lib/utils");
+const Page = require('./helpers/page');
+jest.setTimeout(12000);
 
-let browser, page;
+let page;
 beforeEach(async () => {
-    browser = await puppeteer.launch({
-        headless: false
-    });
-    page = await browser.newPage();
+    page = await Page.build();
     await page.goto('localhost:3000');
-}, 120000);
+});
 
 afterEach(async () => {
-    await browser.close();
-}, 120000);
+    await page.close();
+});
 
 
 test('The header has the correct text', async () => {
+    await page.waitFor('a.brand-logo');
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
     expect(text).toEqual('Blogster');
 });
@@ -28,26 +25,8 @@ test('clicking login starts oauth flow', async () => {
 });
 
 test('When signed in, shows logout button', async () => {
-    const id = '65ce31ee88384d095f432428';
+    await page.login();
 
-    const Buffer = require('safe-buffer').Buffer;
-    const sessionObject = {
-        passport: {
-            user: id
-        }
-    };
-    const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
-
-    const Keygrip = require('keygrip');
-    const keys = require('../config/keys');
-    const keygrip = new Keygrip([keys.cookieKey]);
-    const sig = keygrip.sign('session=' + sessionString);
-
-    await page.setCookie({name: 'session', value: sessionString});
-    await page.setCookie({name: 'session.sig', value: sig});
-
-    await page.goto('localhost:3000');
-    await page.waitFor('a[href="/auth/logout"]');
     const logoutText = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
     expect(logoutText).toEqual('Logout');
 })
